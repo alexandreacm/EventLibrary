ce2db3413d67755c239302d78bbc5fe4f0ed23ca
Alexandre Marques <alexandreacm.marques@gmail.com>
2022-06-27 22:43:27 -0300

    reafctor:Some changes.

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -38,7 +38,7 @@ const styles = StyleSheet.create({
     height: 50,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#787333',
+    backgroundColor: '#6495ED',
     borderRadius: 10,
     marginBottom: 10,
   },
--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,6 @@ import { NativeModules } from 'react-native';
 
 const { Eventlibrary } = NativeModules;
 
-export function multiply(a: number, b: number): Promise<number> {
-  return Eventlibrary.multiply(a, b);
-}
-
 export function onMyEvent(appName: string): Promise<string> {
   return Eventlibrary.onMyEvent(appName);
 }
